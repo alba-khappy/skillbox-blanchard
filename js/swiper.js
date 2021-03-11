c23adb519f7008aa614246115062588079e71bc7
albina <albinabist1993@mail.ru>
2021-03-11 18:02:53 +0300

    Доделывала чекбоксы, секции  "события,издания, контакты"

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,8 @@
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: 'true',
-      type: 'fraction',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    slidesPerView: 1,
   });
+
+
+  
