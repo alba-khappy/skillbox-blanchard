6f775dd62060fad1509bcb6c79e42b0fcfd41780
albina <albinabist1993@mail.ru>
2021-03-23 17:34:48 +0300

    checkbox, slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,27 @@
 const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 1,
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
   
